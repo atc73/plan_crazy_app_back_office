dbe60829b7a328b842e46424c64321103dd9a612
Joris <joris.sellam@gmail.com>
2022-12-09 11:23:18 +0100

    first commit

---
+++ src/main/java/com/example/plan_crazy_app_back_office/connection/PersistenceManager.java
@@ -0,0 +1,20 @@
+package com.example.plan_crazy_app_back_office.connection;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PersistenceManager {
+
+    private static EntityManagerFactory ENTITY_MANAGER;
+
+    private PersistenceManager() {
+
+    }
+
+    public static EntityManagerFactory getEntityManager() {
+        if (ENTITY_MANAGER == null) {
+            ENTITY_MANAGER = Persistence.createEntityManagerFactory("myPU");
+        }
+        return ENTITY_MANAGER;
+    }
+}
